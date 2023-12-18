0cb43df1499d48c85e0fb3528de0f9676b46a8ec
Ionut Gherman <ghermanionut96@gmail.com>
2023-12-18 21:48:43 +0200

    Revert "wahoo: Disable eUICC when GMS and GSF are not installed"
    This reverts commit f7c71908ee883e460d2b45678a3b0e6de893c1c4.

--- device-lineage.mk
+++ device-lineage.mk
@@ -14,10 +14,6 @@ DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-lineage
 PRODUCT_COPY_FILES += \
     device/google/wahoo/allowlist_com.google.android.as.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/allowlist_com.google.android.as.xml
 
-# Parts
-PRODUCT_PACKAGES += \
-    GoogleParts
-
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
